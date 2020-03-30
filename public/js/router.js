8964b64ca2720c5a227115c66004af6838b6f29d
Coen Mathijssen <mathijssen.coen@gmail.com>
2020-03-30 19:02:44 +0200

    changes route

---
+++ public/js/router.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+
+// Rendering shirt designer
+router.get('/', function (req, res) {
+  res.render('user-form.ejs', {
+    generatedID: getRandomNumber(10000),
+    userData: null
+  })
+})
+
+// Rendering shirt designer
+router.get('/shirt-designer', function (req, res) {
+  res.render('user-form.ejs', {
+    generatedID: getRandomNumber(10000),
+    userData: null
+  })
+})
+
+// Rendering saved page
+router.get('/saved/:id', function (req, res) {
+  const id = req.params.id
+  res.render('saved.ejs', {
+    id: id
+  })
+})
+
+// Rendering the form screen for a specific user
+router.get('/shirt-designer/:id', findItem)
+
+async function findItem (req, res) {
+  const id = req.params.id
+  const foundItem = await matchItem(id)
+  res.render('user-form.ejs', {
+    userData: foundItem
+  })
+}
+
+// Code from Menno
+function storeData (data) {
+  const { id, gender, neck, size, color, text, font, textColor } = data
+  const jsonFile = 'data/data.json'
+
+  fs.readFile(jsonFile, (err, content) => {
+    if (err) return console.log(err)
+
+    const contentJSON = JSON.parse(content)
+
+    const formData = { id, gender, neck, size, color, text, font, textColor }
+    console.log('formData: ', formData)
+
+    checkForDuplicateData(contentJSON, formData)
+
+    fs.writeFile(jsonFile, JSON.stringify(contentJSON), err => {
+      if (err) console.log(err)
+    })
+  })
+}
+
+function checkForDuplicateData (contentJSON, formData) {
+  const index = contentJSON.data.findIndex((data) => data.id === formData.id)
+
+  if (index === -1) {
+    console.log('doesnt exist')
+    contentJSON.data.push(formData)
+  } else {
+    console.log('exists')
+    contentJSON.data[index] = formData
+  }
+}
+
+function matchItem (id) {
+  return new Promise((resolve, reject) => {
+    const jsonFile = 'data/data.json'
+    return fs.readFile(jsonFile, (err, content) => {
+      if (err) return console.log(err)
+      const contentJSON = JSON.parse(content)
+      const foundItem = contentJSON.data.filter(found => { return found.id === id })
+      resolve((foundItem.length !== 0 ? foundItem[0] : false))
+    })
+  })
+}
+
+function getRandomNumber (between) {
+  return Math.floor(Math.random() * between) + 1
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express()
 const port = process.env.port || 3000
 const path = require('path')
 
+// Importing files
+const router = require('./public/js/router.js')
+app.use(router)
+
 const fs = require('fs')
 
 // Setting up port for express to use
@@ -14,90 +18,6 @@ app.use(express.static(path.join(__dirname, 'dist')))
 // View Engine init
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-
-// Rendering shirt designer
-app.get('/', function (req, res) {
-  res.render('user-form.ejs', {
-    generatedID: getRandomNumber(10000),
-    userData: null
-  })
-})
-
-// Rendering shirt designer
-app.get('/shirt-designer', function (req, res) {
-  res.render('user-form.ejs', {
-    generatedID: getRandomNumber(10000),
-    userData: null
-  })
-})
-
-// Rendering saved page
-app.get('/saved/:id', function (req, res) {
-  const id = req.params.id
-  res.render('saved.ejs', {
-    id: id
-  })
-})
-
-// Rendering the form screen for a specific user
-app.get('/shirt-designer/:id', findItem)
-
-async function findItem (req, res) {
-  const id = req.params.id
-  const foundItem = await matchItem(id)
-  res.render('user-form.ejs', {
-    userData: foundItem
-  })
-}
-
-// Code from Menno
-function storeData (data) {
-  const { id, gender, neck, size, color, text, font, textColor } = data
-  const jsonFile = 'data/data.json'
-
-  fs.readFile(jsonFile, (err, content) => {
-    if (err) return console.log(err)
-
-    const contentJSON = JSON.parse(content)
-
-    const formData = { id, gender, neck, size, color, text, font, textColor }
-    console.log('formData: ', formData)
-
-    checkForDuplicateData(contentJSON, formData)
-
-    fs.writeFile(jsonFile, JSON.stringify(contentJSON), err => {
-      if (err) console.log(err)
-    })
-  })
-}
-
-function checkForDuplicateData (contentJSON, formData) {
-  const index = contentJSON.data.findIndex((data) => data.id === formData.id)
-
-  if (index === -1) {
-    console.log('doesnt exist')
-    contentJSON.data.push(formData)
-  } else {
-    console.log('exists')
-    contentJSON.data[index] = formData
-  }
-}
-
-function matchItem (id) {
-  return new Promise((resolve, reject) => {
-    const jsonFile = 'data/data.json'
-    return fs.readFile(jsonFile, (err, content) => {
-      if (err) return console.log(err)
-      const contentJSON = JSON.parse(content)
-      const foundItem = contentJSON.data.filter(found => { return found.id === id })
-      resolve((foundItem.length !== 0 ? foundItem[0] : false))
-    })
-  })
-}
-
-function getRandomNumber (between) {
-  return Math.floor(Math.random() * between) + 1
-}
 
 // Express bodyparser data handling
 // configure the app to use bodyParser()
